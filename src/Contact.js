4e8d9efa9d8f192a39de6eb77a8bb9c73d93259d
tanyavohra <vohratanya5@gmail.com>
2026-03-15 12:55:18 +0530

    #

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Contact() {
   return (
-    <div style={{ padding: "1rem" }}>
+    <div style={{ padding: "4 rem" }}>
       <h2>Contact</h2>
       <p>
         If you have feedback, bug reports, or feature requests, reach out .
