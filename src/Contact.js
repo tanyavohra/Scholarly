60773e781326a78754fc22347583d064af258149
tanyavohra <vohratanya5@gmail.com>
2026-03-15 12:58:12 +0530

    ui

--- src/Contact.js
+++ src/Contact.js
@@ -3,19 +3,30 @@ import { Link } from "react-router-dom";
 
 function Contact() {
   return (
-    <div style={{ padding: "4 rem" }}>
+    <div
+      style={{
+        padding: "2rem",
+        marginLeft: "240px",  
+        marginTop: "80px"   
+      }}
+    >
       <h2>Contact</h2>
+
       <p>
-        If you have feedback, bug reports, or feature requests, reach out .
+        If you have feedback, bug reports, or feature requests, reach out.
       </p>
+
       <ul>
         <li>
           Email:{" "}
-          <a href="mailto:vohratanya5@gmail.com">vohratanya5@gmail.com</a>
+          <a href="mailto:vohratanya5@gmail.com">
+            vohratanya5@gmail.com
+          </a>
         </li>
       </ul>
+
       <p>
-        <Link to="/Home">Back to Home</Link>
+        <Link to="/home">Back to Home</Link>
       </p>
     </div>
   );
